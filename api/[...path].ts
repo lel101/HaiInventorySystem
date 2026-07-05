960421cce62db6fd99560869f1b720a89d37a494
lel101 <baidianjhon@gmail.com>
2026-07-05 15:49:23 +0800

    vercel setup API

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,6 @@
 import app from './app';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default app;
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return app(req, res);
+}
